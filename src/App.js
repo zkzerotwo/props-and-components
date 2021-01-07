0f282d6fe3a9d305592f98f7bab224d2364b6092
Christopher 'ZK' Hal <halc713@gmail.com>
2021-01-07 14:07:04 -0600

    Applied solution
    Won't render list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import List from './components/List'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+
+  render() {
+    const { store } = this.props
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
+
+
+// function App(STORE) {
+//   return (
+//     <main className='App'>
+//       <header className='App-header'>
+//         <h1>Trelloyes!</h1>
+//       </header>
+//       <div className='App-List'>
+//         <ListComponent />
+//       </div>
+//     </main>
+//   );
+// }
+
